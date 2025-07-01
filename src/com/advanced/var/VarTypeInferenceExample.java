b2f607f07f46cfbc4b9ef4bbb8adb2eaf3882c12
thilak0805 <daya.22.thilak@gmail.com>
2025-07-01 09:16:17 +0530

    second commit

---
+++ src/com/advanced/var/VarTypeInferenceExample.java
@@ -0,0 +1,25 @@
+package com.advanced.var;
+
+import java.util.List;
+import java.util.Map;
+
+public class VarTypeInferenceExample {
+    public static void main(String[] args) {
+        var list = List.of("dayalan","kumar");
+        System.out.printf("var value is ===="+list);
+        //to iterate the above collection using var
+        for(var name : list){
+            System.out.println("name="+name);
+        }
+
+        //using below ofEntries we can create immutable map
+        // below ofEntries got introduced in java 9
+       var map = Map.ofEntries(Map.entry("a", List.of("apple","banana")));
+        System.out.println("==============");
+        System.out.println("map ="+map);
+        map.forEach((s,  strings) -> {
+            System.out.println("s = "+s);
+            System.out.println("string ="+strings);
+        });
+    }
+}
